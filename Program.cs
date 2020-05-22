bdc86c3ddadbba85b3cbd77e004c59416ceed9c6
fabricio <fnmamao@gmail.com>
2020-05-22 09:45:24 -0300

    adiciona exercicio

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Medidas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int metros;
+            double cm;
+            double km;
+            Console.WriteLine("Entre com a medida em Metros:");
+            metros = Convert.ToInt16(Console.ReadLine());
+
+            km = metros / 1000.0;
+            cm = metros / 0.010000;
+
+            Console.WriteLine("--- Equivalência ---");
+            Console.WriteLine(metros);
+            Console.WriteLine(km);
+            Console.WriteLine(cm);
+        }
+    }
+}
